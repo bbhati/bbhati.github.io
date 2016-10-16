6e3d9c97a41faaa7ccf6c10d8a15f41e75d005aa
bbhati <bbhati@salesforce.com>
2016-10-16 00:10:48 -0700

    my simple analytics app using react and highcharts

---
+++ js/components/chart.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactHighcharts from 'react-highcharts'
+
+let Chart = React.createClass({
+	render() {
+		return <ReactHighcharts config = {this.getChartConfig()}></ReactHighcharts>;
+	},
+
+	getChartConfig() {
+		const year = this.props.year
+		const data = this.props.data
+		const onClick = this.props.onClick
+		const config = {
+			"chart": {
+		      "plotBackgroundColor": null,
+		      "plotBorderWidth": null,
+		      "plotShadow": false,
+		      "type": "pie"
+			  },
+	  		tooltip: {
+            	pointFormat: "{series.name}:{point.y:.1f}%"
+        	},
+
+	  		plotOptions: {
+                pie: {
+                	slicedOffset: 0,
+			        states: {
+			            select: {
+			                color: '#228B22'
+			            },
+			        },
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+	                    enabled: true,
+	                    format: '{point.name}: {point.percentage:.1f} %',
+	                    style: {
+	                        color: 'black'
+	                    }
+                	},
+                    showInLegend: false
+                }
+            },
+      title: {
+        text: 'Revenue by Product,' + year
+      },
+	  "series": [{
+             "data": data,
+              type: 'pie',
+              point:{
+              	events:{
+                  click: function (event) {
+                  		onClick(this.name);	
+                  	}
+              }
+          	}
+          }]
+	}
+		return config;
+	}
+});
+
+export default Chart;
---
+++ js/components/suggestions.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+let Suggestions = React.createClass({
+
+	getInitialState() {
+		return {selected: []}
+	},
+
+	render() {
+		return <div className="suggestions">{this.renderTable()}</div>
+	},
+
+	renderTable() {
+		let rows = this.props.data.map ((datum) => 
+			{
+				let className = "dataRow"
+				return <tr className={className} key={datum} onClick={this.onClick}><td data-obj={datum}>{datum}</td></tr>
+			}
+		)
+		return <table className="rows"><tbody>{rows}</tbody></table>
+	},
+
+	onClick(event) {
+		const target = event.target
+		const key = event.target.dataset.obj
+		this.props.onSelect(key)
+	}
+})
+
+export default Suggestions;
