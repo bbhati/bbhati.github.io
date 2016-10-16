ddca91cf7dceb15287e398e5f9e463b75ccb5dac
bbhati <bbhati@salesforce.com>
2016-10-16 00:23:45 -0700

    fix code formatting

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
-import React from 'react';  
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Container from './components/container.js';
 
 let navigated = function() {
-  let hash = window.location.hash;
-  hash = hash.substring(1)
-  let component = <Container/>
-  switch (hash) {
-    case '\/':
-    case '':
-    component = <Container/>
-    break;
+    let hash = window.location.hash;
+    hash = hash.substring(1)
+    let component = < Container / >
+        switch (hash) {
+            case '\/':
+            case '':
+                component = < Container / >
+                    break;
 
-    default: 
-    component = <div>Not found</div>  
-  }
-  
+            default:
+                component = < div > Not found < /div>  
+        }
+
     ReactDOM.render((
-      component
+        component
     ), document.getElementById("container"));
 }
 
 navigated();
 window.addEventListener('hashchange', navigated, false);
-
-
--- js/components/dropdown.js
+++ js/components/dropdown.js
@@ -3,42 +3,43 @@ import Suggetions from './suggestions.js'
 
 let DropDown = React.createClass({
 
-	propTypes: {
-		onChange: React.PropTypes.func,
-		value: React.PropTypes.number,
-		options: React.PropTypes.array
-	},
-	
-	getInitialState (){
-		return {value: this.props.value, showDropdown: false}
+    propTypes: {
+        onChange: React.PropTypes.func,
+        value: React.PropTypes.number,
+        options: React.PropTypes.array
+    },
 
-	},
+    getInitialState() {
+        return { value: this.props.value, showDropdown: false }
 
-	render () {
-		return <div className="dropdown">
-				<div className="input">
-					<input className="yearInput" value={this.state.value}></input>
-					<button className="downArrow">
-					<i className="fa fa-arrow-down" onClick={this.toggleDropDown}></i></button>
-				</div>
-				{this.renderOptions()}
-			</div>
-	},
+    },
 
-	renderOptions () {
-		const onSelect = (value) => {
-			this.setState({value: value, showDropdown: false})
-			this.props.onSelect(value)
-		};
-		if(this.state.showDropdown)
-			return <div className="result"><Suggetions onSelect={onSelect} data={this.props.options}/></div>
-		else 
-			return undefined
-	},
+    render() {
+        return <div className = "dropdown" >
+            < div className = "input" >
+            < input className = "yearInput"
+        value = { this.state.value } > < /input> < button className = "downArrow" >
+            < i className = "fa fa-arrow-down"
+        onClick = { this.toggleDropDown } > < /i></button >
+            < /div> { this.renderOptions() } < /div>
+    },
 
-	toggleDropDown () {
-		this.setState({showDropdown: !this.state.showDropdown})
-	}
+    renderOptions() {
+        const onSelect = (value) => {
+            this.setState({ value: value, showDropdown: false })
+            this.props.onSelect(value)
+        };
+        if (this.state.showDropdown)
+            return <div className = "result" > < Suggetions onSelect = { onSelect }
+        data = { this.props.options }
+        /></div >
+        else
+            return undefined
+    },
+
+    toggleDropDown() {
+        this.setState({ showDropdown: !this.state.showDropdown })
+    }
 })
 
 export default DropDown;
